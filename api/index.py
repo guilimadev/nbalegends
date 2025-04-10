e4ce52a74ee7ce37b58789fe8f3e49adb8cd028c
Guilherme Lima <guilimadev@gmail.com>
2025-04-09 23:57:19 -0300

    Changes to deploy in vercel 3

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import sys
 from django.core.asgi import get_asgi_application
 
 # Set up Django project path and settings
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager.settings')  # Change if needed
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager.settings')
 
 app = get_asgi_application()
