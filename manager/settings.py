8e8bcde0662b6c4e8f0c2856b7c96418909e2afc
Guilherme Lima <guilimadev@gmail.com>
2025-04-10 00:14:16 -0300

    Changes to deploy in vercel - debug

--- manager/settings.py
+++ manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kt)k2&!6o^vuxqrcc92&jwfwmwf5=cv^jm7l+1q*x-+mlutao%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
