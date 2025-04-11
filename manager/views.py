bcbc288c4f1b8391d869ee0213a391c1e1e6a668
Guilherme Lima <guilimadev@gmail.com>
2025-04-11 15:15:18 -0300

    Correção html Blazers

--- manager/views.py
+++ manager/views.py
@@ -2,7 +2,7 @@ import os
 from django.conf import settings
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from urllib.parse import unquote
+from urllib.parse import unquote, quote
 from django.shortcuts import redirect
 import re
 
@@ -12,7 +12,7 @@ def redirect_to_default_html(request):
 def show_html(request, filename):
     filename = unquote(filename)
     file_path = os.path.join(settings.MEDIA_ROOT, 'htmls', filename)
-    
+
     if not os.path.exists(file_path):
         return HttpResponseNotFound("File not found")
 
@@ -21,19 +21,19 @@ def show_html(request, filename):
 
     def rewrite_href(match):
         href_value = match.group(2)
+        print(href_value)
         if href_value.endswith('.html') and not href_value.startswith(('http', '/', '#')):
-            return f'{match.group(1)}/html-preview/{href_value}{match.group(3)}'
+            encoded_href = quote(href_value)
+            return f'{match.group(1)}/html-preview/{encoded_href}{match.group(3)}'
         return match.group(0)
 
-    # Rewrite relative links to point to the /html-preview/ route
     def rewrite_src(match):
         src_value = match.group(2)
         if not src_value.startswith(('http', '/', 'data:', '#')):
             return f'{match.group(1)}/static/{src_value}{match.group(3)}'
         return match.group(0)
 
-    # Apply both rewrites
-    href_pattern = r'(href\s*=\s*[\'"]?)([^\'"\s>]+)([\'"]?)'
+    href_pattern = r'(href\s*=\s*[\'"])([^\'"]+)([\'"])'
     src_pattern = r'(src\s*=\s*[\'"]?)([^\'"\s>]+)([\'"]?)'
 
     content = re.sub(href_pattern, rewrite_href, content, flags=re.IGNORECASE)
